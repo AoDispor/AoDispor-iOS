26a13ea5d0195ca4f660c992d076d366f753d6db
André Lamelas <andre@lamelas.org>
2016-05-13 15:05:44 +0100

    Initial Commit

---
+++ Ao Dispor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ao Dispor
+//
+//  Created by André Lamelas on 13/05/16.
+//  Copyright © 2016 Ao Dispor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
