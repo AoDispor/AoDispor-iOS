66130d7817ba768fb5d14829eec340619f09bf19
André Lamelas <andre@lamelas.org>
2016-06-01 21:50:06 +0100

    Initial Commit

---
+++ Ao DisporTests/Ao_DisporTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ao_DisporTests.swift
+//  Ao DisporTests
+//
+//  Created by André Lamelas on 01/06/16.
+//  Copyright © 2016 Ao Dispor. All rights reserved.
+//
+
+import XCTest
+@testable import Ao_Dispor
+
+class Ao_DisporTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
